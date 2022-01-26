b38ef2b4b59e656cebabf418fb78491bec072adc
kukucorn <jungheun99@naver.com>
2022-01-27 01:18:35 +0900

    remove: 사용하지 않는 코드 삭제
    - database.js (Store model 생성하는 코드)
    - public-data-parser.js (주석 제거)

--- src/loaders/database.js
+++ src/loaders/database.js
@@ -7,46 +7,6 @@ const sequelize = new Sequelize(database, user, password, {
 	dialect,
 });
 
-const Store = sequelize.define(
-	'Store',
-	{
-		store_id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			autoIncrement: true,
-			primaryKey: true,
-		},
-		name: {
-			type: DataTypes.STRING(100),
-			allowNull: false,
-		},
-		region: {
-			type: DataTypes.STRING(45),
-			allowNull: false,
-		},
-		address: {
-			type: DataTypes.STRING(200),
-			allowNull: false,
-		},
-		contact: {
-			type: DataTypes.STRING(45),
-			defaultValue: null,
-		},
-		category: {
-			type: DataTypes.STRING(45),
-			allowNull: false,
-		},
-		business_hours: {
-			type: DataTypes.STRING(100),
-			allowNull: false,
-		},
-	},
-	{
-		timestamps: false,
-		tableName: 'store',
-	}
-);
-
 (async () => {
 	try {
 		await sequelize.authenticate();
--- src/public_data/public-data-parser.js
+++ src/public_data/public-data-parser.js
@@ -2,9 +2,6 @@ import fs from 'fs';
 
 import iconv from 'iconv-lite';
 import csvtojson from 'csvtojson';
-import mysql from 'mysql2';
-
-import { host, user, password, database } from '../configs/database.js';
 
 const originalCsvFilePath = `${process.cwd()}/src/public_data/assets/jeju_20210615.csv`;
 
@@ -36,51 +33,3 @@ export async function getKindStoreArray() {
 
 	return storeList;
 }
-
-// fs.writeFileSync(`${process.cwd()}/src/public_data/assets/jeju_20210615_utf8.csv`, uft8Content, { encoding: 'utf8' });
-
-// let connection = mysql.createConnection({
-// 	host,
-// 	user,
-// 	password,
-// 	database,
-// });
-
-// connection.connect(async (error) => {
-// 	if (error) console.error(error);
-// 	console.log('connect DB!!!');
-
-// 	let json = await csvtojson().fromString(uft8Content);
-
-// 	const storePropsMap = {
-// 		업소명: 'name',
-// 		업종: 'category',
-// 		지역: 'region',
-// 		연락처: 'contact',
-// 		주소: 'address',
-// 		영업시간: 'business_hours',
-// 		데이터기준일자: 'created_at',
-// 	};
-
-// 	const storeList = json.map((value, index) => {
-// 		let newObj = {};
-// 		for (const key in value) {
-// 			let propName;
-// 			if ((propName = storePropsMap[key])) newObj[propName] = value[key];
-// 		}
-// 		return newObj;
-// 	});
-
-// 	console.log(storeList);
-
-// 	storeList.forEach((store) => {
-// 		const props = Object.keys(store).join(',');
-// 		const values = Object.values(store);
-// 		connection.query({
-// 			sql: `INSERT INTO store (${props}) VALUES (${Array.from(Array(values.length), () => '?').join(',')})`,
-// 			values,
-// 		});
-// 	});
-
-// 	connection.end();
-// });
