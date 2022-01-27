86aa5fb3822c2de4c08526d32b62bc66fbd6d6e8
kukucorn <jungheun99@naver.com>
2022-01-27 16:05:23 +0900

    update: api route에 /api 추가

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(morgan('dev')); // dev format is " :method :url :status :response-time ms - :res[content-length] "
 
-app.use('/store', StoreRouter);
+app.use('/api/stores', StoreRouter);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
