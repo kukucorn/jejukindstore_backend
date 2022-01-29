fcc0e94955e5952c16ad8abcf48ecd75d47b7334
kukucorn <jungheun99@naver.com>
2022-01-30 02:24:32 +0900

    update: process.env.NODE_ENV 에 따라 database 설정 변경
    - export 방식을 export default로 변경함에 따라 import 방식이 변경됨.

--- src/loaders/database.js
+++ src/loaders/database.js
@@ -1,10 +1,10 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-import { database, user, password, host, dialect } from 'configs/database.js';
+import dbConfig from 'configs/database.js';
 
-const sequelize = new Sequelize(database, user, password, {
-	host,
-	dialect,
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+	host: dbConfig.host,
+	dialect: dbConfig.dialect,
 });
 
 (async () => {
