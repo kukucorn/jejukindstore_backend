199d83696c18eb10303901f41b188a6ed423aad1
kukucorn <jungheun99@naver.com>
2022-01-30 02:23:30 +0900

    docs: npm script로 upload-gitignored.ps1 실행

--- scripts/upload-gitignored.ps1
+++ scripts/upload-gitignored.ps1
@@ -1,4 +1,4 @@
 . ./scripts/configs.ps1
 
-scp -r -i $pem_key_loc $source_configs_path $target_user@$target_ip:$target_src_path
-scp -r -i $pem_key_loc $source_public_data_assets_path $target_user@$target_ip:$target_public_data_path
+scp -r -i $pem_key_loc $source_configs_path $target_user@${target_ip}:$target_src_path
+scp -r -i $pem_key_loc $source_public_data_assets_path $target_user@${target_ip}:$target_public_data_path
