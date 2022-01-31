ad1706643d11ecd672b500b24d59d96d0e879e1c
kukucorn <jungheun99@naver.com>
2022-02-01 00:26:20 +0900

    feat: compression middleware 등록

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import helmet from 'helmet';
+import compression from 'compression';
 import cors from 'cors';
 import morgan from 'morgan';
 
@@ -12,6 +13,7 @@ import corsOptions from 'configs/cors-options';
 const app = express();
 
 app.use(helmet());
+app.use(compression());
 app.use(cors(corsOptions));
 app.use(morgan('dev')); // dev format is " :method :url :status :response-time ms - :res[content-length] "
 
