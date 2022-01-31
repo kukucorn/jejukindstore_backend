af33f72784f36ad8121d4fd28f7527933fb9788d
kukucorn <jungheun99@naver.com>
2022-01-31 22:23:41 +0900

    feat: cors 설정

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import { StoreRouter } from 'apis';
 
 import { port } from 'configs/server';
+import corsOptions from 'configs/cors-options';
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(morgan('dev')); // dev format is " :method :url :status :response-time ms - :res[content-length] "
 
 app.use('/api/stores', StoreRouter);
