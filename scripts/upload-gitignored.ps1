114e6f553629508dfb214b6a93512f92676ba7fe
kukucorn <jungheun99@naver.com>
2022-02-02 01:22:31 +0900

    comment: upload-gitignored.ps1 주석 추가

--- scripts/upload-gitignored.ps1
+++ scripts/upload-gitignored.ps1
@@ -1,4 +1,7 @@
 . ./scripts/configs.ps1
 
+# upload /configs to remote
 scp -r -i $pem_key_loc $source_configs_path $target_user@${target_ip}:$target_src_path
+
+# upload /public_data/assets to remote
 scp -r -i $pem_key_loc $source_public_data_assets_path $target_user@${target_ip}:$target_public_data_path
